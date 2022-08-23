eb23db16218003a4c806fef154afc931c323f593
KaustavKumarKhanikar <80395137+Hungryfoxz@users.noreply.github.com>
2022-08-23 22:27:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,15 +19,3 @@ function App() {
 
 export default App;
 
-
-// 837712663d9d3fd685b2c5350d44d384 api
-/* export async function load({ fetch }){
-  console.log(1)
-  const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=837712663d9d3fd685b2c5350d44d384&language=en-US&page=1`);
-  const data = await res.json();
-  console.log(11)
-  if(res.ok){
-    return { popular: data.results };
-  }
-}
-*/
